ec613ae6423f97ae6638afe1f04bed742a2b21ab
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-09-17 09:33:23 +0100

    Added route groups

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <p className="text-6xl">Dashboard</p>
+        </div>
+    )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+
+const LandingPage = () => {
+    return (
+        <div>
+            Landing page (unprotected)
+        </div>
+    )
+}
+
+export default LandingPage
