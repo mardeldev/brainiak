4ee377576d601dde6e66480bcf20df2af90aabc2
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-09-17 11:48:19 +0100

    Added dashboard link in sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
-import { Sidebar } from "lucide-react";
+import Sidebar from "@/components/sidebar";
+
 
 const DashboardLayout = ({
     children
@@ -8,7 +9,7 @@ const DashboardLayout = ({
 }) => {
     return (
         <div className="h-full relative">
-            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-[#160f29]">
                 <div>
                     <Sidebar />
                 </div>
