aabe30f18b984d0580e44f386b80ee555b91b378
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-09-25 22:09:45 +0100

    Video generation done 📹

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,6 +1,17 @@
+"use client"
 
-import Image from "next/image"
+import { usePathname } from "next/navigation"
+import Image from "next/image";
+
 export const Loader = () => {
+    const path = usePathname();
+    let pageText = "Brainiak is thinking..."
+
+
+    if (path == "/music" || path == "/video") {
+        pageText = "Brainiak is thinking. Please be patient if this is your first time using this generator. It takes a couple of minutes for the stars and the servers to align..."
+    }
+
     return (
         <div className="h-full flex flex-col gap-y-4 items-center justify-center">
             <div className="w-10 h-10 relative animate-spin">
@@ -11,7 +22,7 @@ export const Loader = () => {
                 />
             </div>
             <p className="text-sm text-muted-foreground">
-                Brainiak is thinking...
+                {pageText}
             </p>
         </div>
     )
